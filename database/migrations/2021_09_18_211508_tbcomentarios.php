79818e18ca5404b4d7db0e9d6822dd5d5dae9edc
Daniel Takabaiashi <dahminoru@gmail.com>
2021-09-18 18:22:41 -0300

    migrations comentarios adicionada

---
+++ database/migrations/2021_09_18_211508_tbcomentarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Tbcomentarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email');
+            $table->timestamp('data');
+            $table->text('comentario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
